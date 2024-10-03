618bf8a995d6d67f176f3b3a7bbee4d616e36b06
nutcas3 <mauricee423@gmail.com>
2024-10-04 02:15:47 +0300

    Add homepage layout with background image and header icons; include welcome text and button for entering 'DEX' ISLAND

---
+++ app/src/HomePage.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import SpeakerIcon from './component/Icons/Speaker';
+import UnionIcon from './component/Icons/Union';
+import MenuBarIcon from './component/Icons/MenuBar';
+
+const HomePage = () => {
+  return (
+    <div className="relative w-full h-screen">
+      <div className="absolute top-0 left-0 w-full h-full">
+        <Image
+          src="/pinacoladaisland.png"
+          layout="fill"
+          objectFit="cover"
+          alt="Pina Colada Island"
+        />
+      </div>
+
+      <div className="absolute top-0 left-0 w-full flex justify-between items-center p-4">
+        <div className="flex justify-start">
+          <UnionIcon />
+        </div>
+
+        <div className="flex justify-center">
+          <MenuBarIcon />
+        </div>
+
+        <div className="flex justify-end">
+          <SpeakerIcon />
+        </div>
+      </div>
+
+      <div className="relative z-1 flex flex-col items-center justify-between h-full text-center">
+        <div className="flex flex-col items-center justify-center h-full">
+          <span className="block text-5xl font-extrabold text-[#b8ff4f]">
+            Welcome to
+          </span>
+
+          <div className="relative inline-block text-[7vw] font-extrabold text-white">
+            PIÑA COLADA
+            <span className="absolute right-0 -bottom-10 text-[5vw] text-[#b8ff4f]">
+              ISLAND
+            </span>
+
+            <div className="absolute left-0 -bottom-16 text-base text-white text-left">
+              <span>Your favourite DEX</span>
+              <br />
+              <span>with a Caribbean flavour</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-40">
+          <button className="px-8 py-3 text-lg font-bold text-black bg-white rounded-[8px] shadow-lg hover:bg-[#e6b805] transition duration-300">
+            Enter the &apos;DEX&apos; ISLAND
+          </button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default HomePage;
