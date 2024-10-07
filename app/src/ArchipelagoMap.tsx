8eeab643f64cd0d805ab15a14b3dab56ba32303f
nutcas3 <mauricee423@gmail.com>
2024-10-07 19:22:18 +0300

    archipelago ustyled yet still WIP

--- app/src/ArchipelagoMap.tsx
+++ app/src/ArchipelagoMap.tsx
@@ -1,13 +1,12 @@
-// archipelagoMap.tsx
 import React from 'react';
 import IslandOne from './component/Icons/Archipelago/islandOne';
-// import IslandTwo from './component/Icons/Archipelago/islandTwo';
-// import IslandThree from './component/Icons/Archipelago/islandThree';
-// import IslandFour from './component/Icons/Archipelago/islandFour';
-// import IslandFive from './component/Icons/Archipelago/islandFive';
-// import IslandSix from './component/Icons/Archipelago/islandSix';
-// import IslandSeven from './component/Icons/Archipelago/islandSeven';
-// import IslandEight from './component/Icons/Archipelago/islandEight';
+import IslandTwo from './component/Icons/Archipelago/islandTwo';
+import IslandThree from './component/Icons/Archipelago/islandThree';
+import IslandFour from './component/Icons/Archipelago/islandFour';
+import IslandFive from './component/Icons/Archipelago/islandFive';
+import IslandSix from './component/Icons/Archipelago/islandSix';
+import IslandSeven from './component/Icons/Archipelago/islandSeven';
+import IslandEight from './component/Icons/Archipelago/islandEight';
 
 interface ArchipelagoMapProps {
     onIslandHover: (islandName: string) => void;
@@ -15,18 +14,66 @@ interface ArchipelagoMapProps {
 }
 
 const ArchipelagoMap: React.FC<ArchipelagoMapProps> = ({ onIslandHover, onIslandLeave }) => (
-    <div>
-        <IslandOne
-            onHover={() => onIslandHover('Daiquiri Island,\nA Liquid Staking destination coming soon.')}
-            onLeave={onIslandLeave}
-        />
-        {/* <IslandTwo onHover={() => onIslandHover('Mojito Bay')} onLeave={onIslandLeave} />
-        <IslandThree onHover={() => onIslandHover('Pina Colada Island')} onLeave={onIslandLeave} />
-        <IslandFour onHover={() => onIslandHover('Rum Island')} onLeave={onIslandLeave} />
-        <IslandFive onHover={() => onIslandHover('Tequila Shores')} onLeave={onIslandLeave} />
-        <IslandSix onHover={() => onIslandHover('Whiskey Isle')} onLeave={onIslandLeave} />
-        <IslandSeven onHover={() => onIslandHover('Vodka Valley')} onLeave={onIslandLeave} />
-        <IslandEight onHover={() => onIslandHover('Gin Grove')} onLeave={onIslandLeave} /> */}
+    <div 
+  
+    >
+        {/* Position each island absolutely based on the map's layout */}
+        <div 
+            style={{}} // Adjust positions accordingly
+            onMouseEnter={() => onIslandHover('Daiquiri Island,\nA Liquid Staking destination coming soon.')}
+            onMouseLeave={onIslandLeave}
+        >
+            <IslandOne className="cursor-pointer" />
+        </div>
+        <div 
+            style={{  }} // Adjust positions accordingly
+            onMouseEnter={() => onIslandHover('Mojito Bay')}
+            onMouseLeave={onIslandLeave}
+        >
+            <IslandTwo />
+        </div>
+        <div 
+            style={{  }} // Adjust positions accordingly
+            onMouseEnter={() => onIslandHover('Pina Colada Island')}
+            onMouseLeave={onIslandLeave}
+        >
+            <IslandThree />
+        </div>
+        <div 
+            style={{ }} // Adjust positions accordingly
+            onMouseEnter={() => onIslandHover('Rum Island')}
+            onMouseLeave={onIslandLeave}
+        >
+            <IslandFour className="cursor-pointer" />
+        </div>
+        <div 
+            style={{ position: 'absolute', top: '45%', left: '15%' }} // Adjust positions accordingly
+            onMouseEnter={() => onIslandHover('Tequila Shores')}
+            onMouseLeave={onIslandLeave}
+        >
+            <IslandFive  />
+        </div>
+        <div 
+            style={{  }} // Adjust positions accordingly
+            onMouseEnter={() => onIslandHover('Whiskey Isle')}
+            onMouseLeave={onIslandLeave}
+        >
+            <IslandSix/>
+        </div>
+        <div 
+            style={{  }} // Adjust positions accordingly
+            onMouseEnter={() => onIslandHover('Vodka Valley')}
+            onMouseLeave={onIslandLeave}
+        >
+            <IslandSeven />
+        </div>
+        <div 
+            style={{ }} // Adjust positions accordingly
+            onMouseEnter={() => onIslandHover('Gin Grove')}
+            onMouseLeave={onIslandLeave}
+        >
+            <IslandEight />
+        </div>
     </div>
 );
 
