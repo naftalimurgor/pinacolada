50de9701cd20bdb546d924c3c2ee10be8369a7d4
nutcas3 <mauricee423@gmail.com>
2024-10-08 18:43:40 +0300

    pages of pool swap and token

---
+++ app/pools/page.tsx
@@ -0,0 +1,11 @@
+import Tabs from "../src/component/Tabs";
+
+const PoolPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <Tabs />
+    </div>
+  )
+};
+
+export default PoolPage;
---
+++ app/token/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TokenInterface from '../src/TokenInterface';
+
+const TokenPage: React.FC = () => {
+  return (
+    <TokenInterface />
+  );
+};
+
+export default TokenPage;
