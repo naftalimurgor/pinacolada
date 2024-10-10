78482579e54f77265391a4a6ddee11dfd574e7e9
nutcas3 <mauricee423@gmail.com>
2024-10-10 19:52:30 +0300

    changed the iocns to match the ones in designs

--- app/src/component/Icons/Slider.tsx
+++ app/src/component/Icons/Slider.tsx
@@ -7,10 +7,18 @@ interface IconProps {
 
 const SliderIcon: React.FC<IconProps> = ({ className }) => {
     return (
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-            <path d="M12 4L4 12" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M4 4L12 12" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}> 
+            <path d="M3 14L3 10" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M3 6.66667L3 2" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M8 14L8 8" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M8 5V2" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M13 14V11" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M13 8L13 2" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1 10H5" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M6 5L10 5" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M11 11H15" stroke="white" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
+
     );
 };
 
