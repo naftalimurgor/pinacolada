70904c73a108bfcaa1221f6f78a8413b31c64a7a
nutcas3 <mauricee423@gmail.com>
2024-11-01 17:49:51 +0300

    Feat :UI Matched archipelago design of the map

--- app/src/ArchipelagoMap.tsx
+++ app/src/ArchipelagoMap.tsx
@@ -14,65 +14,147 @@ interface ArchipelagoMapProps {
 }
 
 const ArchipelagoMap: React.FC<ArchipelagoMapProps> = ({ onIslandHover, onIslandLeave }) => (
-    <div 
-  
-    >
-        {/* Position each island absolutely based on the map's layout */}
-        <div 
-            style={{}} // Adjust positions accordingly
-            onMouseEnter={() => onIslandHover('Daiquiri Island,\nA Liquid Staking destination coming soon.')}
+    <div className="relative w-full h-full">
+        {/* Each island positioned precisely as per design */}
+        <div
+            className="absolute cursor-pointer"
+            style={{
+                top: '90px',
+                left: '150px'
+            }}
+            onMouseEnter={() => onIslandHover('Daiquiri Island, A Liquid Staking destination coming soon.')}
             onMouseLeave={onIslandLeave}
         >
-            <IslandOne className="cursor-pointer" />
+            <IslandOne onHover={function (): void {
+                throw new Error('Function not implemented.');
+            } } onLeave={function (): void {
+                throw new Error('Function not implemented.');
+            } } />
         </div>
-        <div 
-            style={{  }} // Adjust positions accordingly
+
+        <div
+            className="absolute cursor-pointer"
+            style={{
+                width: '227.28px',
+                height: '234.62px',
+                top: '310px',
+                left: '260px'
+            }}
             onMouseEnter={() => onIslandHover('Mojito Bay')}
             onMouseLeave={onIslandLeave}
         >
-            <IslandTwo />
+            <IslandTwo onHover={function (): void {
+                throw new Error('Function not implemented.');
+            } } onLeave={function (): void {
+                throw new Error('Function not implemented.');
+            } } />
         </div>
-        <div 
-            style={{  }} // Adjust positions accordingly
+
+        <div
+            className="absolute cursor-pointer"
+            style={{
+                width: '150px',
+                height: '190px',
+                top: '280px',
+                left: '45px'
+            }}
             onMouseEnter={() => onIslandHover('Pina Colada Island')}
             onMouseLeave={onIslandLeave}
         >
-            <IslandThree />
+            <IslandThree onHover={function (): void {
+                throw new Error('Function not implemented.');
+            } } onLeave={function (): void {
+                throw new Error('Function not implemented.');
+            } } />
         </div>
-        <div 
-            style={{ }} // Adjust positions accordingly
+        <div
+            className="absolute cursor-pointer"
+            style={{
+                width: '180px', // last one 
+                height: '180px',
+                top: '30px', 
+                left: '720px' 
+            }}
             onMouseEnter={() => onIslandHover('Rum Island')}
             onMouseLeave={onIslandLeave}
         >
-            <IslandFour className="cursor-pointer" />
+            <IslandFour onHover={function (): void {
+                throw new Error('Function not implemented.');
+            } } onLeave={function (): void {
+                throw new Error('Function not implemented.');
+            } } />
         </div>
-        <div 
-            style={{ position: 'absolute', top: '45%', left: '15%' }} // Adjust positions accordingly
+
+        <div
+            className="absolute cursor-pointer"
+            style={{
+                width: '140px',
+                height: '140px',
+                top: '-30px',
+                left: '510px'
+            }}
             onMouseEnter={() => onIslandHover('Tequila Shores')}
             onMouseLeave={onIslandLeave}
         >
-            <IslandFive  />
+            <IslandFive onHover={function (): void {
+                throw new Error('Function not implemented.');
+            } } onLeave={function (): void {
+                throw new Error('Function not implemented.');
+            } } />
         </div>
-        <div 
-            style={{  }} // Adjust positions accordingly
+
+        <div
+            className="absolute cursor-pointer"
+            style={{
+                width: '160px',
+                height: '160px',
+                top: '120px',
+                left: '550px'
+            }}
             onMouseEnter={() => onIslandHover('Whiskey Isle')}
             onMouseLeave={onIslandLeave}
         >
-            <IslandSix/>
+            <IslandSix onHover={function (): void {
+                throw new Error('Function not implemented.');
+            } } onLeave={function (): void {
+                throw new Error('Function not implemented.');
+            } } />
         </div>
-        <div 
-            style={{  }} // Adjust positions accordingly
+
+        <div
+            className="absolute cursor-pointer"
+            style={{
+                width: '140px',
+                height: '140px',
+                top: '-25px',
+                left: '400px'
+            }}
             onMouseEnter={() => onIslandHover('Vodka Valley')}
             onMouseLeave={onIslandLeave}
         >
-            <IslandSeven />
+            <IslandSeven onHover={function (): void {
+                throw new Error('Function not implemented.');
+            } } onLeave={function (): void {
+                throw new Error('Function not implemented.');
+            } } />
         </div>
-        <div 
-            style={{ }} // Adjust positions accordingly
+
+        <div
+            className="absolute cursor-pointer"
+            style={{
+                width: '130px',
+                height: '130px',
+                top: '100px',
+                left: '360px'
+            }}
             onMouseEnter={() => onIslandHover('Gin Grove')}
             onMouseLeave={onIslandLeave}
         >
-            <IslandEight />
+            <IslandEight onHover={function (): void {
+                throw new Error('Function not implemented.');
+            } } onLeave={function (): void {
+                throw new Error('Function not implemented.');
+            } } />
         </div>
     </div>
 );
