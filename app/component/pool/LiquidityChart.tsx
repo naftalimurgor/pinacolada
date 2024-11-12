f6be6cf301b6dedd2786c1980bdbeabbf06ea4ad
nutcas3 <mauricee423@gmail.com>
2024-11-13 01:48:36 +0300

    Fixed static page issue with Use client directive

--- app/component/pool/LiquidityChart.tsx
+++ app/component/pool/LiquidityChart.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react';
 import ChartButtons from './ChartButtons';
 import LiquidityChartComponent from './LiquidityChartComponent';
--- app/component/pool/PoolDetail.tsx
+++ app/component/pool/PoolDetail.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from "react";
 import PoolInfo from "./PoolInfo";
 import LiquidityChart from "./LiquidityChart";
